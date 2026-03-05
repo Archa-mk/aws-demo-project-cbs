37938bacebb04650fe3540a5781114a1d32e06ab
Archa-mk <archamkishore@gmail.com>
2026-03-05 05:47:48 +0530

    Step run

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,18 @@
+resource "aws_iam_role" "step_function_role" {
+  name = "etl-step-function-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "states.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
 resource "aws_iam_role_policy" "step_function_policy" {
   role = aws_iam_role.step_function_role.id
 
@@ -17,6 +32,3 @@ resource "aws_iam_role_policy" "step_function_policy" {
   })
 }
 
-resource "aws_iam_role" "step_function_role" {
-  name = "etl-step-function-role"
-}
